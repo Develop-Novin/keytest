7cfdbf7b0d7fdd8517dde9fd504fd91a3d5851f9
DevelopHezarehno <85205284+DevelopHezarehno@users.noreply.github.com>
2022-01-20 02:29:11 -0500

    Update app.js

--- public/js/app.js
+++ public/js/app.js
@@ -6,7 +6,7 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-var API_KEY = "83481f5faamsh2ad632a8c173090p17e910jsn26ace8e4558c";
+var API_KEY = EACT_APP_TODO;
 'use strict';
 
 var Friends = function (_React$Component) {
